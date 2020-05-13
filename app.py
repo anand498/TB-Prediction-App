fba88fbb7a94a13515071d1d90009c4382ef5857
anand498 <anand.pandey803@gmail.com>
2020-05-13 19:19:03 +0530

    adding the lung crop script

--- app.py
+++ app.py
@@ -26,10 +26,53 @@ def get_model():
     model=load_model('C:/Users/anand/Downloads/model128v5.h5')   
     print("model_loaded")
     return model
+def Equalization(img):
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    equalized_img = clahe.apply(img)
+    return equalized_img
+def Resize(img, size):
+    img = img[:,1:-1]
+    resized_image = cv2.resize(img, (size, size)) 
+    return resized_image
+def processimg(img):
+    #     imgagePath='C:/Users/anand/Desktop/px27.jpg'
+#     img = cv2.imread(imgagePath)
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    nbTrue=0
+    n=0
+
+    resized_image = Resize(img,360)
+    equ = Equalization(resized_image)
+    crop = CroppingLowerRegion(equ)
+
+    height, width = crop.shape[:2]
+
+    left, right = LungBoundary(crop) ### Binary Thresholding #Otsu Thresholding
+    ret, thresholdImg = cv2.threshold(crop, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    thresholdImg = cv2.bitwise_not(thresholdImg)
+
+    LungAreaImg = np.zeros((height, width, 1), np.uint8)
+    LungAreaImg = LungArea(LungAreaImg, left, right)
+    crop=cv2.resize(crop,(360,360))
+    return crop
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+def predict(imgpath):
+    img = cv2.imread(imgpath)
+    img= processimg(img)
+    img = img.astype('uint8')
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    img = img_to_array(img)
+    img = cv2.resize(img,(360,360))
+    img=img.reshape(1,360,360,3)
+    img = img.astype('float32')
+    img = img / 255.0
+    result = model.predict_classes(img)
+    pred=model.predict(img)
+    return classes[result[0]]
 
 
 @app.route("/upload", methods=['POST'])
@@ -46,10 +89,6 @@ def upload():
     img = cv2.imread(destination)
     cv2.imwrite('static/xray/file.png',img)
     img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    # equalized_img = clahe.apply(img)
-    # # Save with the same name in another folder.
-    # # plt.imshow(equalized_img)
    img = img.astype('uint8')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
